336e413afba384a22f3f0a954ab19091fcce9abb
Dan Bernier <danbernier@gmail.com>
2017-11-13 22:14:13 -0500

    add some reference links for gh api, and fix index lineheight

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var sketches;
 function preload() {
+  // https://developer.github.com/v3/repos/contents/
+  // https://stackoverflow.com/questions/25022016/get-all-file-names-from-a-github-repo-through-the-github-api#25023044
+  // https://developer.github.com/v3/
   var url = 'https://api.github.com/repos/danbernier/slow-photo-sketches/contents/sketches';
   sketches = loadJSON(url);
-  console.log(sketches);
 }
 
 function setup() {
   for (i in sketches) {
     var link = createA('sketch.html?sketch=' + sketches[i].name, sketches[i].name).class('sketchLink');
-    link.position(50, 30 + (i * 30));
+    link.position(50, 30 + (i * 40));
   }
 }
