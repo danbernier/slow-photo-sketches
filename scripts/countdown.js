ab00f02d0bc310c5a3af1a54dfd4dc44c66808e1
Dan Bernier <danbernier@gmail.com>
2017-11-24 18:35:13 -0500

    Experiment: a countdown function

---
+++ scripts/countdown.js
@@ -0,0 +1,32 @@
+function inCountdown(drawFn) {
+  background(0);
+
+  if (timeLeft() > 0) {
+    push();
+    noStroke();
+    fill(30, 60, 30);
+    var size = map(timeLeft(), 5000, 2000, 300, 0);
+    ellipse(width/2, height/2, max(0, size), max(0, size));
+    pop();
+  }
+  else {
+    drawFn();
+  }
+}
+
+function startCountdown() {
+  this.countdownStart = millis();
+}
+
+function timeLeft() {
+  var timeSpent = 0;
+  if (this.countdownStart) {
+    timeSpent = millis() - this.countdownStart;
+  }
+  return 5000 - timeSpent;
+}
+
+
+function mousePressed() { // also called by touchStarted
+  startCountdown();
+}
---
+++ sketches/cubes.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(1200, 720);
+  rectMode(CENTER);
+  background(0);
+  fill(255, 0, 0);
+  noStroke();
+}
+
+function draw() {
+  inCountdown(function() {
+    // Say we want a row of red cubes. How about 10 of them, over 30 seconds?
+    // Then we should display about 1 every 3 seconds.
+    var progress = millis() / 30000.0;
+
+    if (progress <= 1) {
+      var inSquareProgress = progress % (1.0 / 10);
+      if (inSquareProgress > 0.035 && inSquareProgress < 0.065) {
+        rect(width/2, height/2, 450, 450);
+      }
+      else {
+        background(0);
+      }
+    }
+  });
+}
