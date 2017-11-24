ea52f6774f8a07ce99ea8b4d436d94876ae410de
Dan Bernier <danbernier@gmail.com>
2017-11-24 17:58:15 -0500

    TURN IT OFF!

--- sketches/click-test.js
+++ sketches/click-test.js
@@ -1,9 +1,11 @@
 function setup() {
   createCanvas(1200, 720);
   this.drawCircle = false;
+  background(0);
 }
 
 function draw() {
+  background(0);
   if (this.drawCircle) {
     fill(90, 200, 90);
     ellipse(width * 0.5, height * 0.5, 600, 600);
