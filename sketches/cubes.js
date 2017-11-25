60d83fda7ecce7b4aa7e89631a724efa5217a3d0
Dan Bernier <danbernier@gmail.com>
2017-11-24 19:32:39 -0500

    Don't need to check progress var in a sketch now

--- sketches/cubes.js
+++ sketches/cubes.js
@@ -11,14 +11,12 @@ function draw() {
     // Say we want a row of red cubes. How about 10 of them, over 30 seconds?
     // Then we should display about 1 every 3 seconds.
 
-    if (progress <= 1) {
-      var inSquareProgress = progress % (1.0 / 10);
-      if (inSquareProgress > 0.035 && inSquareProgress < 0.065) {
-        rect(width/2, height/2, 600, 600);
-      }
-      else {
-        background(0);
-      }
+    var inSquareProgress = progress % (1.0 / 10);
+    if (inSquareProgress > 0.035 && inSquareProgress < 0.065) {
+      rect(width/2, height/2, 600, 600);
+    }
+    else {
+      background(0);
     }
   });
 }
